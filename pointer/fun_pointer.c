18a97a224019934e48140df7ae61708a3cb771d9
tidalmelon <tidal.melon@gmail.com>
2019-03-14 09:38:42 +0800

    function pointer

--- pointer/fun_pointer.c
+++ pointer/fun_pointer.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
 int fun1(char *name, int age) {
-    printf("my name is , age: %d", age);
+    printf("age: %d\n", age);
+
     int num = 0;
-    size_t i;
-    for (i=0;;) {
+
+    while(*name!='\0') {
+
+        printf("%c, %c, %d\n", *name, *name-32, *name);
+
         num += *name;
         name ++;
     }
+
     return num;
 }
 
@@ -18,14 +23,13 @@ int main() {
     ////声明一个指针类型: 去掉名字就是指针类型， 去掉*名字 就是指针指向的类型。 明显指向一个函数int (char*, int)
     int (*pfun1)(char*, int);
     pfun1 = fun1;
-    printf("output: 1\n");
 
     ////通过指针调用函数
-    char name[] = "abcdef";
+    char name[] = "jeff";
     int age = 18;
     int a = (*pfun1)(name, age);
 
-    //printf("output: %d", a);
+    printf("num: %d\n", a);
 
     return 0;
 }
