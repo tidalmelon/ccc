2b4406d05dd817242853dd3b16a16e9cbe6c49e2
tidalmelon <tidal.melon@gmail.com>
2019-08-12 21:14:13 +0800

    pointer add sub

---
+++ pointer/struct_add_sub.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef struct list_node {
+    char a;
+    char b;
+} list_node;
+
+int main() {
+    list_node node = {'a', 'b'};
+    printf("%c %d\n", node.a, node.a);
+
+    list_node *ptr = &node;
+
+    char *ptr_a = &(node.a);
+
+    char *ptr_b = &(node.b);
+
+    printf("%p\n%p\n\n", ptr, ptr_a);
+
+    printf("%p\n%p\n", ptr, ptr_b);
+    
+    printf("size of list_node = %d\n", sizeof(node));
+    printf("size of list_node * = %d\n", sizeof(ptr));
+
+    return 0;
+}
+
